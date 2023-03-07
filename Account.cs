50951725f126f5c1ed1336fe5dc452f401bc0ae9
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-03-06 21:06:57 -0800

    Update

---
+++ Account.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Prog_122_W23_Lecutre_14_Enumerable
+{
+    internal class Account
+    {
+
+        // To declare a enuemrable
+        // enum
+        public enum AccountType { Savings, Checking, Investment }
+
+        string _accountNumber;
+        AccountType _typeOfAccount;
+        string _name;
+        decimal _balance;
+        BitmapImage _customerPicture;
+
+        decimal _withdrawFee = 200;
+
+
+        // Difference between savings and checking accounts
+        // Required Min Balance
+        // Interest Rate
+        // Withdraw Fee
+
+        public Account(AccountType typeOfAccount, string name, decimal balance, string filePath)
+        {
+            Random rand = new Random();
+            // Create a "fake" unique number
+            _accountNumber = rand.Next(100000000, 1000000000).ToString();
+            _typeOfAccount = typeOfAccount;
+            _name = name;
+            _customerPicture = ConvertCustomerImage(filePath);
+
+        } // Account()
+
+        public BitmapImage CustomerPicture { get => _customerPicture; set => _customerPicture = value; }
+
+
+        public void Withdraw(decimal amount)
+        {
+            if(amount < _balance)
+            {
+                _balance -= _withdrawFee;
+            }
+            else
+            {
+                if(_typeOfAccount == AccountType.Checking)
+                {
+                    _balance -= _withdrawFee;
+                }
+            }
+
+        }
+
+        private BitmapImage ConvertCustomerImage(string filePath)
+        {
+            Uri uri = new Uri(filePath);
+            // Pass URI into BitMapImage
+            BitmapImage bitmap = new BitmapImage(uri);
+            return bitmap;
+        }
+
+        public override string ToString()
+        {
+            return $"{_accountNumber} - {_typeOfAccount} - {_balance}";
+        }
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,78 @@ namespace Prog_122_W23_Lecutre_14_Enumerable
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Account> accounts = new List<Account>();
+
         public MainWindow()
         {
             InitializeComponent();
+            PopulateAccountTypes();
+            
+        }
+
+        public void PopulateAccountTypes()
+        {
+            cmbAccountTypes.Items.Add("Savings");
+            cmbAccountTypes.Items.Add("Checking");
+            cmbAccountTypes.Items.Add("Investment");
+
+            cmbAccountTypes.SelectedIndex = 0;
+        }
+
+        // Inside my button, when clicked, creates a new instance of an account with the required information.
+        // Adds it to the account list
+        // Updates the account listbox display
+        private void btnAddAccount_Click(object sender, RoutedEventArgs e)
+        {
+            string fullName = txtName.Name;
+            decimal balance = decimal.Parse(txtBalance.Text);
+            int accountType = cmbAccountTypes.SelectedIndex;
+            Account.AccountType convertedType = (Account.AccountType)accountType;
+
+
+            // What is casting?
+            string filePath = txtFilePath.Text;
+
+            // A new instance of _____ type
+            // new Type()
+            Account account = new Account(convertedType, fullName,balance, filePath);
+
+            accounts.Add(account);
+
+            DisplayAccounts();
+        }
+
+        public void DisplayAccounts()
+        {
+            lbAccounts.Items.Clear();
+
+            foreach (Account a in accounts)
+            {
+                lbAccounts.Items.Add(a);
+            }
+        }
+
+        private void btnSelectPicture_Click(object sender, RoutedEventArgs e)
+        {
+            // Open file diagloug
+            OpenFileDialog op = new OpenFileDialog();   
+            if (op.ShowDialog() == true)
+            {
+                txtFilePath.Text = op.FileName;
+            }
+        }
+        int selectedIndex = 0;
+        private void lbAccounts_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (lbAccounts.SelectedIndex > -1)
+            {
+                selectedIndex = lbAccounts.SelectedIndex;
+            }
+
+            imgCustomer.Source = accounts[selectedIndex].CustomerPicture;
+
         }
     }
+
 }
